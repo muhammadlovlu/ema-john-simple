e6371a4b9734b1dc6b0f68d225bd8e25aba1e72a
muhammadlovlu <chyshahab@gmail.com>
2021-04-15 22:20:07 +0600

    updated

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Shop.css';
+const Shop = () => {
+    return (
+        <div>
+            <h1>Macbook Air 256GB</h1>
+            <h4></h4>
+        </div>
+    );
+};
+
+export default Shop;
